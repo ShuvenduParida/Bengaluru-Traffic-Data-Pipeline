aaa207d58f58c28f588e0648cccbfd5533341cf0
ShuvenduParida <220301120096@cutm.ac.in>
2026-08-21 11:57:36 +0530

    jdbc connection

--- src/config/JDBC_connection.py
+++ src/config/JDBC_connection.py
@@ -5,24 +5,41 @@ from pyspark.sql import SparkSession
 
 load_dotenv()
 
-
 host = os.getenv("REDSHIFT_HOST")
 port = os.getenv("REDSHIFT_PORT")
 database = os.getenv("REDSHIFT_DATABASE")
 username = os.getenv("REDSHIFT_USER")
 password = os.getenv("REDSHIFT_PASSWORD")
 
-
 jdbc_url = f"jdbc:redshift://{host}:{port}/{database}"
+
+jar_path = (
+    "C:/Users/suppo/OneDrive/Desktop/HTML email template test/"
+    "Traffic_Pipeline/drivers/redshift-jdbc42-2.2.8.jar"
+)
+
+print("JAR path:")
+print(jar_path)
+
+print("\nJAR exists:")
+print(os.path.exists(jar_path))
+
+if not os.path.exists(jar_path):
+    raise FileNotFoundError(
+        f"Redshift JDBC driver not found:\n{jar_path}"
+    )
 
 spark = (
     SparkSession.builder
     .appName("RedshiftConnectionTest")
+    .config("spark.driver.host", "127.0.0.1")
+    .config("spark.driver.bindAddress", "127.0.0.1")
+    .config("spark.jars", jar_path)
+    .config("spark.driver.extraClassPath", jar_path)
     .getOrCreate()
 )
 
 print("\nConnecting to Redshift...\n")
-
 
 df = (
     spark.read
@@ -31,12 +48,12 @@ df = (
     .option("dbtable", "daily_traffic_data.location")
     .option("user", username)
     .option("password", password)
-    .option("driver", "com.amazon.redshift.jdbc.Driver")
+    .option("driver", "com.amazon.redshift.Driver")
     .load()
 )
 
-
 print("Connection successful!")
+
 print("\nSchema:")
 df.printSchema()
 
@@ -45,8 +62,5 @@ df.show(10, truncate=False)
 
 print("\nTotal rows:")
 print(df.count())
-
-
-
 
 spark.stop()
